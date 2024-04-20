58e5b839f977ee2301749e42ee9767e574598a31
unknown <shivhamyelgire@outlook.com>
2024-04-21 02:15:19 +0530

    update

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from io import BytesIO
 import base64
 from flask_cors import CORS
 import json
+import os
 
 app = Flask(__name__)
 #CORS(app, origins="http://localhost:3000")
@@ -139,4 +140,5 @@ def create_payment_session():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
